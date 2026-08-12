f213d06ebd60393617e409a0cb8abf876b9ee561
zengmaoyuan <zengmaoyuan0505@163.com>
2026-08-13 07:46:12 +0800

    test

---
+++ src/my.js
@@ -0,0 +1,21 @@
+let savedTheme = localStorage.getItem('theme');
+
+if (savedTheme == 'D') {
+    $('.ui').toggleClass('inverted');
+}
+
+$('#turnLight').on('click', function () {
+    if ($('.moon.icon').length != 0) $('.moon.icon').addClass('sun').removeClass('moon');
+    else $('.sun.icon').addClass('moon').removeClass('sun');
+    $('.ui').toggleClass('inverted');
+    savedTheme = (savedTheme == 'D') ? 'L' : 'D';
+    localStorage.setItem('theme', savedTheme);
+});
+
+$('#copy-content').on('click', function () {
+    let $this = $(this);
+    navigator.clipboard.writeText($this.text()).then(() => {
+        $this.addClass('blue');
+        setTimeout(() => { $this.removeClass('blue'); }, 100);
+    });
+});
